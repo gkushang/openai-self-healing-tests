111ccc35ee78a1652893c2b5ff1423c7b249e326
Kushang Gajjar <g.kushang@gmail.com>
2025-01-03 14:01:54 -0500

    openai self healing example

--- tests/steps/github.steps.ts
+++ tests/steps/github.steps.ts
@@ -8,7 +8,7 @@ Before(() => {
 When(/he logs into Github/, () => {
     // pause();
     githubLoginPage.login('xyz', '****');
-    I.wait(2); //hard-coded wait to see the actions for demo
+    I.wait(10); //hard-coded wait to see the actions for demo
 });
 
 Given(/Fred is on Github LoginPage/, () => {
