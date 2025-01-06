49d53fd3a25f52dfc06f35ff2e8d31a1a857081b
Kushang Gajjar <g.kushang@gmail.com>
2025-01-06 13:05:10 -0500

    openai self healing example

--- tests/pages/github/github-login.page.ts
+++ tests/pages/github/github-login.page.ts
@@ -10,12 +10,14 @@ class HomePage {
 
     login(username, password) {
 
-        I.wait(10)
-        I.say('waiting for this demo....', 'blue');
+        I.say('\nwaiting for this demo....', 'blue');
+        I.wait(5)
 
         I.fillField(this.locators.usernameField, username);
         I.fillField(this.locators.passwordField, password);
 
+        I.say('\ndemo: now, it is trying to click the INVALID locator and tests should throw an error. Then, tests will send a PROMPT to OpenAI for a suggested fix....', 'blue');
+        I.wait(5)
         // this will fail due to INVALID locator
         I.click(this.locators.signInButton);
     }
